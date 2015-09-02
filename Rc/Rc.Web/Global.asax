3df56a53e653464e1617b0c65c0ca8413a11770a
Dmitry Goncharov <dgoncharov@aexsoft.com>
2015-09-02 11:12:20 +0600

    add mvc+webapi project

---
+++ Rc/Rc.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Rc.Web.MvcApplication" Language="C#" %>
---
+++ Rc/Rc.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rc.Web.Startup))]
+namespace Rc.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
