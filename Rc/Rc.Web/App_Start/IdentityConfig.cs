f1559c7217ac0ea34e16c0f6daf3dbf7fca0c281
Dmitry Goncharov <dgoncharov@aexsoft.com>
2015-09-02 11:27:18 +0600

    rework UserManager to use RavenDB instead of EF

--- Rc/Rc.Web/App_Start/IdentityConfig.cs
+++ Rc/Rc.Web/App_Start/IdentityConfig.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using Rc.Web.Models;
+using RavenDB.AspNet.Identity;
+using Rc.Web.App_Start;
 
 namespace Rc.Web
 {
@@ -42,7 +42,7 @@ namespace Rc.Web
 
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) 
         {
-            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(()=> { return AppHelper.Store.OpenSession(); }));
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
